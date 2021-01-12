54c2d928e0188da6cf615faeea64fa48e533b475
angelocaisip <angelo.caisip080076@my.lsgh.edu.ph>
2021-01-12 13:49:14 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-atnpbi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-atnpbi)
---
+++ pages/home/home.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  height: number;
+  weight: number;
+  bmi: number;
+  bmiMessage: String;
+
+  constructor(public navCtrl: NavController) {}
+
+  calculateBMI() {
+    this.bmi = this.weight / (this.height * this.height);
+    this.bmi = parseFloat(this.bmi.toFixed(2));
+
+    if (this.bmi <18.5) {
+      this.bmiMessage = "underweight";
+    } else if (this.bmi > 18.5 && this.bmi < 25){
+      this.bmiMessage = "normal"
+    } else if (this.bmi > 25 && this.bmi < 30){
+      this.bmiMessage = "overweight";
+    } else {
+      this.bmiMessage = "obese";
+    }
+
+  }
+}
+
